1f1c5f1b74a82820bb88e1b09e9024f0d37e3ff2
Martin Hettiger <martin@hettiger.com>
2018-04-21 11:52:55 +0200

    Refactoring and init animation improvements

--- index.ts
+++ index.ts
@@ -1,101 +1,101 @@
 import * as d3 from 'd3';
 import * as cloud from 'd3-cloud';
+import { Tag } from './tag.interface';
+export { Tag } from './tag.interface';
 
 d3.layout.cloud = cloud;
-declare var window: any;
 
 export class D3JsWordcloud {
-    static init(targetElement: HTMLDivElement, ratio: number, tags: {"key": string, "value": number}[]) {
-        var fill = d3.scale.category20b();
 
-        var w = targetElement.parentElement.offsetWidth,
-            h = targetElement.parentElement.offsetWidth * ratio;
+    protected svg;
 
-        var max,
-            fontSize;
+    protected vis;
 
-        var layout = d3.layout.cloud()
+    protected fill;
+
+    protected layout;
+
+    protected fontSize;
+
+    constructor(
+        protected targetElement: HTMLDivElement,
+        protected ratio: number,
+        protected tags: Tag[]
+    ) {
+        this.init();
+    }
+
+    update(): void {
+        this.layout.font('impact').spiral('archimedean');
+        this.fontSize = d3.scale['sqrt']().range([10, 100]);
+
+        if (this.tags.length) {
+            this.fontSize.domain([+this.tags[this.tags.length - 1].value || 1, +this.tags[0].value]);
+        }
+
+        this.layout.stop().words(this.tags).start();
+    }
+
+    protected init(): void {
+        this.fill = d3.scale.category20b();
+        const w = this.targetElement.parentElement.offsetWidth;
+        const h = this.targetElement.parentElement.offsetWidth * this.ratio;
+
+        this.layout = d3.layout.cloud()
             .timeInterval(Infinity)
             .size([w, h])
-            .fontSize(function(d) {
-                return fontSize(+d.value);
-            })
-            .text(function(d) {
-                return d.key;
-            })
-            .on("end", draw);
+            .fontSize((d) => this.fontSize(+d.value))
+            .text((d) => d.key)
+            .on('end', this.draw.bind(this));
 
-        var svg = d3.select(targetElement).append("svg")
-            .attr("width", w)
-            .attr("height", h);
+        this.svg = d3.select(this.targetElement).append('svg')
+            .attr('width', w)
+            .attr('height', h);
 
-        var vis = svg.append("g").attr("transform", "translate(" + [w >> 1, h >> 1] + ")");
+        this.vis = this.svg.append('g').attr('transform', 'translate(' + [w >> 1, h >> 1] + ')');
 
-        update();
+        this.update();
 
-        if(window.attachEvent) {
-            window.attachEvent('onresize', update);
+        if ((<any>window).attachEvent) {
+            (<any>window).attachEvent('onresize', this.update.bind(this));
+        } else if (window.addEventListener) {
+            window.addEventListener('resize', this.update.bind(this));
         }
-        else if(window.addEventListener) {
-            window.addEventListener('resize', update);
-        }
+    }
 
-        function draw(data, bounds) {
-            var w = targetElement.parentElement.offsetWidth,
-                h = targetElement.parentElement.offsetWidth * ratio;
+    protected draw(data, bounds): void {
+        const w = this.targetElement.parentElement.offsetWidth;
+        const h = this.targetElement.parentElement.offsetWidth * this.ratio;
+        this.svg.attr('width', w).attr('height', h);
 
-            svg.attr("width", w).attr("height", h);
+        const scale = bounds ? Math.min(
+            w / Math.abs(bounds[1].x - w / 2),
+            w / Math.abs(bounds[0].x - w / 2),
+            h / Math.abs(bounds[1].y - h / 2),
+            h / Math.abs(bounds[0].y - h / 2)) / 2 : 1;
 
-            const scale = bounds ? Math.min(
-                w / Math.abs(bounds[1].x - w / 2),
-                w / Math.abs(bounds[0].x - w / 2),
-                h / Math.abs(bounds[1].y - h / 2),
-                h / Math.abs(bounds[0].y - h / 2)) / 2 : 1;
+        const text = this.vis.selectAll('text').data(data, (d) => d.text.toLowerCase());
 
-            var text = vis.selectAll("text")
-                .data(data, function(d) {
-                    return d.text.toLowerCase();
-                });
-            text.transition()
-                .duration(1000)
-                .attr("transform", function(d) {
-                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-                })
-                .style("font-size", function(d) {
-                    return d.size + "px";
-                });
-            text.enter().append("text")
-                .attr("text-anchor", "middle")
-                .attr("transform", function(d) {
-                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-                })
-                .style("font-size", function(d) {
-                    return d.size + "px";
-                })
-                .style("opacity", 1e-6)
-                .transition()
-                .duration(1000)
-                .style("opacity", 1);
-            text.style("font-family", function(d) {
-                return d.font;
-            })
-                .style("fill", function(d) {
-                    return fill(d.text.toLowerCase());
-                })
-                .text(function(d) {
-                    return d.text;
-                });
+        text.transition()
+            .duration(1000)
+            .attr('transform', (d) => 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')')
+            .style('font-size', (d) => d.size + 'px');
 
-            vis.transition().attr("transform", "translate(" + [w >> 1, h >> 1] + ")scale(" + scale + ")");
-        }
+        text.enter()
+            .append('text')
+            .style('opacity', 1e-6)
+            .transition()
+            .duration(1000)
+            .attr('text-anchor', 'middle')
+            .attr('transform', (d) => 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')')
+            .style('font-size', (d) => d.size + 'px')
+            .style('opacity', 1);
 
-        function update() {
-            layout.font('impact').spiral('archimedean');
-            fontSize = d3.scale['sqrt']().range([10, 100]);
-            if (tags.length){
-                fontSize.domain([+tags[tags.length - 1].value || 1, +tags[0].value]);
-            }
-            layout.stop().words(tags).start();
-        }
+        text.style('font-family', (d) => d.font)
+            .style('fill', (d) => this.fill(d.text.toLowerCase()))
+            .text((d) => d.text);
+
+        this.vis.transition()
+            .attr('transform', 'translate(' + [w >> 1, h >> 1] + ')scale(' + scale + ')');
     }
 }
